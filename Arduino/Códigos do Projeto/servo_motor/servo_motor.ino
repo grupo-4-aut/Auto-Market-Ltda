8cc9cc3fb46f745db0b4b850706965f293ce7365
Grupo 4 <109996195+grupo-4-aut@users.noreply.github.com>
2022-09-17 16:10:03 -0300

    Add files via upload

---
+++ Arduino/Códigos do Projeto/servo_motor/servo_motor.ino
@@ -0,0 +1,54 @@
+//SERVO MOTOR
+#include <Servo.h>
+
+#define SERVO 6
+
+Servo s; // Variável Servo
+int pos; // Posição Servo
+//SERVO MOTOR
+
+//SENSOR INFRAVERMELHO
+const int pinoLed = 4; //PINO DIGITAL UTILIZADO PELO LED
+const int pinoSensor = 8; //PINO DIGITAL UTILIZADO PELO SENSOR
+//SENSOR INFRAVERMELHO
+
+void setup() {
+  //SENSOR INFRAVERMELHO
+  pinMode(pinoSensor, INPUT); //DEFINE O PINO COMO ENTRADA
+  pinMode(pinoLed, OUTPUT); //DEFINE O PINO COMO SAÍDA
+  digitalWrite(pinoLed, LOW); //LED INICIA DESLIGADO
+  //SENSOR INFRAVERMELHO
+
+  //SERVO MOTOR
+  s.attach(SERVO);
+  Serial.begin(9600);
+  s.write(0);
+  //SERVO MOTOR
+}
+
+void loop() {
+  //SENSOR INFRAVERMELHO
+  if (digitalRead(pinoSensor) == LOW) { //SE A LEITURA DO PINO FOR IGUAL A LOW, FAZ
+    digitalWrite(pinoLed, HIGH); //ACENDE O LED
+    //SERVO MOTOR
+    delay(1000);
+    if (digitalRead(pinoSensor) == LOW) {
+      for (pos = 0; pos < 90; pos++)
+      {
+        s.write(pos);
+        delay(5);
+      }
+      delay(1000);
+      for (pos = 90; pos >= 0; pos--)
+      {
+        s.write(pos);
+        delay(5);
+      }
+    }
+    //SERVO MOTOR
+
+  } else { //SENÃO, FAZ
+    digitalWrite(pinoLed, LOW); //APAGA O LED
+  }
+  //SENSOR INFRAVERMELHO
+}
